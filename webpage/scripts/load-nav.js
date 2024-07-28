26a632c0ad68ece98af095d588b33d73dad18f79
joe <joseph.satow@gmail.com>
2024-07-28 18:22:02 +0000

    faq added

--- webpage/scripts/load-nav.js
+++ webpage/scripts/load-nav.js
@@ -1,12 +1,28 @@
-$(function () {
-    $("#nav-placeholder").load("includes/navbar.html");
-});
-
 function open_nav_menu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
       x.style.display = "none";
-    } else {
+  } else {
       x.style.display = "block";
-    }
   }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetch('includes/navbar.html')
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById('nav-placeholder').innerHTML = data;
+          document.getElementById('open-nav-menu').addEventListener('click', function(event) {
+            event.preventDefault();
+            open_nav_menu();
+        });
+      })
+      .catch(error => {
+          console.error('There was a problem with the fetch operation:', error);
+      });
+});
